03109fb14cece79efeafed551448e977fac1bd79
Vickopiyo <victor.okoth@student.moringaschool.com>
2022-05-15 17:32:46 +0300

    activates alert function

--- Assets/JS/index.js
+++ Assets/JS/index.js
@@ -1,21 +1,24 @@
-<script type="text/javascript">
+
     
-            function result() {
-                
-            }{
-                 var score = 0;
-                 if(document.getElementById('correct1').checked){
-                    score++;
+function result(){
+    var score=0;
+    if (document.getElementById('correct1').checked)
+    {
+        score++;
+    }
+    if (document.getElementById('correct2').checked)
+    {
+        score++;
+    }
+    if (document.getElementById('correct3').checked)
+    {
+        score++;
+    }
+    alert("you score is "+ score/3*100);
+    math.round(score);
+    
+}
 
-                 }
-                 if(document.getElementById('correct2').checked){
-                     score++;
-                 }
-                 if(document.getElementById('correct3').checked){
-                     score++;
-                 }
-                 alert("YOUR SCORE:" +score)
-                 
-            }
+            
 
-        </script>
+        
