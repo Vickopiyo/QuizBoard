698dcf35472e4bfc34c64aff6322759233507b41
Vickopiyo <victor.okoth@student.moringaschool.com>
2022-05-15 14:57:43 +0300

    sets a radio as a possible answer not all three

---
+++ Assets/JS/index.js
@@ -0,0 +1,18 @@
+<script type="text/javascript">
+    
+            function result(){
+                 var tot = 0;
+                 if(document.getElementById('correct1').checked){
+                     tot++;
+                 }
+                 if(document.getElementById('correct2').checked){
+                     tot++;
+                 }
+                 if(document.getElementById('correct3').checked){
+                     tot++;
+                 }
+                 document.write("YOUR SCORE:" +tot)
+                 
+            }
+
+        </script>
