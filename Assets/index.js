d74e1d600b77d15a065dea21cfb6f8854e657505
Vickopiyo <victor.okoth@student.moringaschool.com>
2022-05-16 08:11:10 +0300

    Adds js functions

---
+++ Assets/index.js
@@ -0,0 +1,26 @@
+function results(){
+    var score = 0;
+    if(document.getElementById('correct1').checked){
+        score++;
+    }
+    if(document.getElementById('correct2').checked){
+        score++;
+    }
+
+    if(document.getElementById('correct3').checked){
+    score++;
+
+    }
+  
+
+
+
+
+
+
+
+
+
+
+
+}
